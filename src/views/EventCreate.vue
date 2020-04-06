b1acc5a427a37852d0b712cf4f2b443b251f5093
joelquek <joel.qcx@gmail.com>
2020-04-06 21:57:57 +0900

    computed property for userName

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Create Event</h1>
+  <div><h2>Create Event, {{ userName }}</h2>
+  <p>This event is created by {{ userName }}</p>
+  </div>
 </template>
+<script>
+export default {
+  computed:{
+    userName(){
+      return this.$store.state.user.name
+    }
+  }
+}
+</script>
