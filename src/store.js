d94401bca1ea1c5f87e118b84d5810b51e6f5fba
joelquek <joel.qcx@gmail.com>
2020-04-06 21:57:12 +0900

    create user

--- src/store.js
+++ src/store.js
@@ -4,7 +4,9 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {id: 'user_001', name: 'User 001'}
+  },
   mutations: {},
   actions: {}
 })
