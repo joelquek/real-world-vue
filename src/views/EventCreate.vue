c05f47596ae76f2d9b56c12b1c18f5ff27eafa58
joelquek <joel.qcx@gmail.com>
2020-04-13 23:04:08 +0900

    added form

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,29 +1,73 @@
 <template>
-  <div>
-    <h2>Create Event, {{ user.name }}</h2>
-    <p>This event is created by {{ user.name }}</p>
-    <button @click="incrementCount">Increment</button>
-    <input type="number" v-model.number="incrementBy">
-  </div>
+  <form>
+    <label>Select a category</label>
+    <select v-model="event.category">
+      <option v-for="cat in categories" :key="cat">{{cat}}</option>
+    </select>
+    <h3>Name describe your event</h3>
+    <div>
+      <label>Title</label>
+      <input v-model="event.title" type="text" placeholder="Add an event title" />
+    </div>
+    <div class="field">
+      <label>Description</label>
+      <input v-model="event.description" type="text" placeholder="Add a description" />
+    </div>
+    <h3>Where is your event?</h3>
+    <div class="field">
+      <label>Location</label>
+      <input v-model="event.location" type="text" placeholder="Add a location" />
+    </div>
+    <h3>When is your event?</h3>
+    <div class="field">
+      <label>Date</label>
+      <datepicker v-model="event.date" placeholder="Select a date" />
+    </div>
+    <div class="field">
+      <label>Select a time</label>
+      <select v-model="event.time">
+        <option v-for="time in times" :key="time">{{ time }}</option>
+      </select>
+    </div>
+    <input type="submit" class="button -fill-gradient" value="Submit">
+  </form>
 </template>
 <script>
-import { mapState } from 'vuex'
-import { mapGetters } from 'vuex'
-
+import Datepicker from 'vuejs-datepicker'
 export default {
-  data(){
-    return {
-      incrementBy: 1
-      }
+  components:{
+    Datepicker
   },
-  computed: {
-    ...mapState(['categories', 'user']),
-    ...mapGetters(['categoriesLength','getEventById'])
+  data(){
+    const times = []
+    for(let i = 1; i<=24; i++){
+      times.push(i+ ':00')
+    }
+    return {
+      event: this.createFreshEvent(),
+      times: times,
+      categories: this.$store.state.categories,
+    }
   },
   methods:{
-    incrementCount(){
-      this.$store.dispatch('updateCount', this.incrementBy)
+    createFreshEvent(){
+      const user = this.$store.state.user
+      return{
+        category: '',
+        organizer: user,
+        title: '',
+        description:'',
+        location: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
     }
   }
 }
 </script>
+<style scoped>
+  .field{
+    margin-bottom: 24px;
+  }
+</style>
