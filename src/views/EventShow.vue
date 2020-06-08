56a9928cbc8b5bc72a17f05142dbc666ee127522
joelquek <joel.qcx@gmail.com>
2020-06-08 22:34:54 +0900

    eventShow use namespace

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -30,7 +30,7 @@ import {mapState, mapActions} from 'vuex'
 export default {
   props: ['id'], //https://router.vuejs.org/guide/essentials/passing-props.html
   created() {
-    this.fetchEvent(this.id)
+    this.$store.dispatch('event/fetchEvent',this.id)
   },
   computed: mapState({
     event: state => state.event.event
