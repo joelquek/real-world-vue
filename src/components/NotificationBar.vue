a5584eba8083c664b90fd2c92402dc222c67b461
Joel Quek <joel.qcx@gmail.com>
2020-06-13 13:19:08 +0900

    autoclose notificationBar

--- src/components/NotificationBar.vue
+++ src/components/NotificationBar.vue
@@ -17,7 +17,19 @@ export default {
         notificationTypeClass(){
             return `-text-${this.notification.type}`
         }
-    }
+    },
+    data(){
+        return{
+            timeout: null
+        }
+    },
+    mounted(){
+        this.timeout = setTimeout(()=> this.remove(this.notification),5000)
+    },
+    beforeDestroy(){
+        clearTimeout(this.timeout)
+    },
+    methods: mapActions('notification', ['remove'])
 }
 </script>
 
