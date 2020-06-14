aa63e156626585c8173761b5cae68825dcf1160a
Joel Quek <joel.qcx@gmail.com>
2020-06-14 13:31:58 +0900

    axios interceptors

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import NProgress from 'nprogress' //import nprogress
 
 const apiClient = axios.create({
   baseURL: `http://localhost:3000`,
@@ -8,6 +9,17 @@ const apiClient = axios.create({
     'Content-Type': 'application/json'
   }
 })
+
+apiClient.interceptors.request.use(config=>{
+  NProgress.start()
+  return config
+})
+
+apiClient.interceptors.response.use(response =>{
+  NProgress.done()
+  return response
+})
+
 
 export default {
   getEvents(perPage, page) {
